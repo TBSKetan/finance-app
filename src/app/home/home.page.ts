a577399e202209a2c420e814db37ba8f2444c355
Nikunj Munjiyasara <tbs.nikunjm@gmail.com>
2022-09-09 15:33:52 +0530

    Impliment dashbord designs and add screebshot

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  // set dynamic color set here
+  features: any[] = [
+      {id:1, name:'Top Up', src:'assets/icon/top_up.svg', background:'rgba(56, 128, 255,0.1)',  page:''},
+      {id:2, name:'Withdraw', src:'assets/icon/cash_withdrawal.svg',background:'rgba(61, 194, 255,0.1)', page:''},
+      {id:3, name:'Send', src:'assets/icon/send.svg',background:'rgba(255, 196, 9,0.1)', page:''},
+      {id:4, name:'Pay', src:'assets/icon/debit_card.svg',background:'rgba(235, 68, 90,0.1)', page:''},
+  ];
+
+  transaction: any[] = [
+    {id: 1, vendor: 'Recived from PhonePe', image: '', amount: 1500, time:'3:00 PM'},
+    {id: 2, vendor: 'Recived from Paytm', image: '', amount: 5000, time:'7:30 AM'},
+    {id: 3, vendor: 'Recived from GPay', image: '', amount: -500, time:'6:00 PM'},
+    {id: 4, vendor: 'Recived from Ketan', image: '', amount: 2500, time:'8:00 PM'},
+    {id: 5, vendor: 'Recived from Hiren', image: '', amount: 15500, time:'5:00 PM'},
+  ];
   constructor() {}
 
 }
